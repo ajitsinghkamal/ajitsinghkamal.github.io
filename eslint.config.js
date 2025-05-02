4248c35b26f73f9535808d95fe97d0083b1b4d7b
Ajit Singh Kamal <ajitskamal@gmail.com>
2025-05-02 08:55:30 +0200

    feat: content update and lint

--- eslint.config.js
+++ eslint.config.js
@@ -6,13 +6,30 @@ import stylisticJs from "@stylistic/eslint-plugin-js";
 
 export default tseslint.config(
     eslint.configs.recommended,
-    eslintPluginAstro.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
     stylisticJs.configs.all,
     stylisticTs.configs.all,
     {
         "languageOptions": {
             "parserOptions": {
+                "projectService": true,
+                "tsconfigRootDir": import.meta.dirname
+            }
+        }
+    },
+    {
+        "files": [
+            "*.astro",
+            "**/*.astro/*.ts",
+            "*.astro/*.ts"
+        ],
+        "extends": [eslintPluginAstro.configs.recommended],
+        "languageOptions": {
+            "parser": "astro-eslint-parser",
+            "processor": "astro/client-side-ts",
+            "parserOptions": {
+                "parser": tseslint.parser,
+                "extraFileExtensions": [".astro"],
                 "projectService": true,
                 "tsconfigRootDir": import.meta.dirname
             }
