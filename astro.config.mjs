4bf4c70de61d4ed3be2acb6b692540d715e65753
Ajit Singh Kamal <ajitskamal@gmail.com>
2025-05-03 01:33:15 +0200

    fix: base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    "site": "ajitsinghkamal.github.io",
+    "site": "https://ajitsinghkamal.github.io",
     "base": "/ajitsinghkamal",
     "integrations": [
         mdx(),
